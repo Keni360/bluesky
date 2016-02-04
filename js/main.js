4acaff80023c803ce3988535ce1c6f72df7dea7f
Mauro Vieira - net <maurosilva.aj@gmail.com>
2016-02-04 13:31:00 -0200

    Adicionado slide show

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-
+window.onload = function(){
+    
         var teste = document.querySelector('#nuvens');
     
     
@@ -10,7 +11,7 @@
                 this.elemento = document.querySelector(id);
                 this.posX = this.elemento.style.left;
                     
-                 /*metodo reponsável pela animação da nuvem*/
+                 //metodo reponsável pela animação da nuvem
                 this.mover = function(x,y){
                     
                     this.posX+=1;
@@ -29,6 +30,7 @@
             
             var nuvem1 = new nuvem("#nuvem1");
             var nuvem2 = new nuvem("#nuvem2");
+                nuvem2.posX = 650;
             var nuvem3 = new nuvem("#nuvem3");
             var nuvem4 = new nuvem("#nuvem4");
             function update(){
@@ -40,7 +42,73 @@
 
             setInterval(update,16);
                 
+   /*================================================
+    *   Slideshow 
+    * ===============================================
+    */
+   
+         var Slideshow = function(elemento){
+            this.elemento = elemento;
+            this.slideshow = document.querySelector(elemento);
+            this.slideshow.childElementCount;
+            this.i = 1;
+            this.slideAtual = document.querySelector(elemento+" :nth-child("+this.i+")");
+            this.totalSlides = this.slideshow.childElementCount;
+            this.ultimoSlide = document.querySelector(elemento+":last-child");
+            
+             this.playSlide = function(){
+                 
+                this.slideAtual = document.querySelector(elemento+":nth-child("+this.i+")");
+                 
+                 
+                 //Se o slide atual for o primeiro
+                 if(this.slideAtual.previousElementSibling == null){
+                    //escondento ultimo slide  
+                    this.ultimoSlide.style.left = "100%";
+                    //mostre o primeiro slide
+                    this.slideAtual.style.left = "0%";
+                    
+                    this.i++;
+                 }
+                 
+                 //Se ainda houver mais slides
+                 if(this.slideAtual.previousElementSibling != null)
+                 {
+                    //esconda o slide anterior
+                    this.slideAtual.previousElementSibling.style.left = "100%";
+                    
+                    //mostre o slide atual
+                    this.slideAtual.style.left = "0%";
+                    
+                    this.i++;
+                    
+                 }
+                 //Se não houver mais slides 
+                 if(this.slideAtual.nextElementSibling == null)
+                 {
+                     //zere o contador
+                     this.i =1; 
+                     
+                     
+                 }
+                 
+                 
+             }
+             
+             
+            }
+                
+
+        //var slideOutdoor = new Slideshow("#slideshow");
+        //document.querySelector(slideOutdoor.elemento+" :nth-child(1)").style.opacity = 1;
+        var slide = new Slideshow("#slideshow section");
         
-            
-            
-            
+        setInterval(function(){
+            slide.playSlide();        
+        },8000)
+        
+        
+        alert(slide.slideAtual);
+        
+    
+    }   
