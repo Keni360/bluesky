4c34328843ffd59794078f390b5e0c34d40b6156
Mauro Vieira - net <maurosilva.aj@gmail.com>
2016-02-24 10:38:19 -0300

    Imagens otimizadas, adcionado core.css e sistema de sprites nos livros da sessao conhecimentos(sobremim)

--- email.php
+++ email.php
@@ -1,31 +1,28 @@
 <?php
 	$destino = "keni360@gmail.com, maurosilva.aj@gmail.com, maurin182@hotmail.com, contato@mauroweb.com.br";
-	$remetente = $_POST['email'];
-	$nome = $_POST['nome'];
-	$telefone = $_POST['telefone'];
-	$msg = $_POST['msg'];
+	$remetente = isset($_POST['email'])?$_POST['email']:null;
+	$nome = isset($_POST['nome'])?$_POST['nome']:null;
+	$telefone = isset($_POST['telefone'])?$_POST['telefone']:null;
+	$msg = isset($_POST['msg'])?$_POST['msg']." $telefone, $nome":null;
 	$assunto = "Contato através do site (mauroweb.com.br)";
 	
 	$headers  = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
-    $headers .= "From: $nome <$remetente>";
+    	$headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+   	$headers .= "From: $nome <$remetente>";
 		
-		$enviaremail = mail($destino, $assunto, $msg, $headers);
-		
-	if($enviaremail){
-		echo("email enviado com sucesso");
-		
+		if($nome != null && $email != null && $msg != null){
+                mail($destino, $assunto, $msg, $headers);
+                $nome = null;
+                $email = null;
+                $msg = null;
 		echo("<script> 
-						
-						document.onload = window.history.back();
-			</script>");
-		
-	}else{
-		echo("Erro");
+			document.onload = window.history.back();
+		</script>");
+            }else{
 		echo("<script> 
-						
-						document.onload = window.history.back();
-			</script>");
-		
-	}
+			document.onload = window.history.back();
+		</script>");
+		}
+
 ?>
+
